bc5e04c3ddac0f3855414332dad61d1f51648830
Jenifer Cochran <Jenifer.Cochran@rgi-corp.com>
2017-04-22 10:32:21 -0400

    adding a start and end point location

--- RoutingAlgorithmProject/RoutingAlgorithmProject/MainWindow.xaml.cs
+++ RoutingAlgorithmProject/RoutingAlgorithmProject/MainWindow.xaml.cs
@@ -10,6 +10,22 @@ namespace RoutingAlgorithmProject
         public MainWindow()
         {
             InitializeComponent();
-        }   
+        }
+
+        private void MyMapView_MapViewTapped(object sender, Esri.ArcGISRuntime.Controls.MapViewInputEventArgs e)
+        {
+            var viewModel = DataContext as MainWindowViewModel;
+            if(viewModel != null)
+            {
+                if(viewModel.IsMovingStartPoint)
+                {
+                    viewModel.StartLocation = e.Location;
+                }
+                else
+                {
+                    viewModel.EndLocation = e.Location;
+                }
+            }
+        }
     }
 }
--- RoutingAlgorithmProject/RoutingAlgorithmProject/MainWindowViewModel.cs
+++ RoutingAlgorithmProject/RoutingAlgorithmProject/MainWindowViewModel.cs
@@ -2,14 +2,16 @@
 using GalaSoft.MvvmLight.Command;
 using GalaSoft.MvvmLight;
 using RoutingAlgorithmProject.Utility;
+using Esri.ArcGISRuntime.Geometry;
 
 namespace RoutingAlgorithmProject
 {
-     public class MainWindowViewModel : ViewModelBase
+    public class MainWindowViewModel : ViewModelBase
     {
 
         public MainWindowViewModel()
         {
+            IsMovingStartPoint = true;
             FindRouteAStarCommand = new RelayCommand(AStarCommandExecuted);
             FindRouteDijikstraCommand = new RelayCommand(DijikstraCommandExecuted);
         }
@@ -23,6 +25,56 @@ namespace RoutingAlgorithmProject
         {
             OsmUtility.ReadOsmData();
         }
+
+        public string MovingPointText
+        {
+            get
+            {
+                return IsMovingStartPoint ? "Move Start Point" : "Move End Point";
+            }
+        }
+
+        private bool _isMovingStartPoint;
+        public bool IsMovingStartPoint
+        {
+            get { return _isMovingStartPoint; }
+            set
+            {
+                _isMovingStartPoint = value;
+                RaisePropertyChanged(() => IsMovingStartPoint);
+                RaisePropertyChanged(() => MovingPointText);
+            }
+        }
+
+        private MapPoint _startLocation;
+        private MapPoint _endLoaction;
+
+        public MapPoint StartLocation
+        {
+            get
+            {
+                return _startLocation;
+            }
+            set
+            {
+                _startLocation = value;
+                RaisePropertyChanged(() => StartLocation);
+            }
+        }
+
+        public MapPoint EndLocation
+        {
+            get
+            {
+                return _endLoaction;
+            }
+            set
+            {
+                _endLoaction = value;
+                RaisePropertyChanged(() => EndLocation);
+            }
+        }
+
 
         public RelayCommand FindRouteAStarCommand { get; private set; }
         public RelayCommand FindRouteDijikstraCommand { get; private set; }
