480c299b4df3a5c79ee4be9089044a1ec359f039
somethingd <somethingd@gmail.com>
2017-04-22 16:35:03 -0400

    added DC pbf file. fixed OsmSharp References

--- RoutingAlgorithmProject/RoutingAlgorithmProject/Utility/OsmUtility.cs
+++ RoutingAlgorithmProject/RoutingAlgorithmProject/Utility/OsmUtility.cs
@@ -1,10 +1,6 @@
-﻿using Itinero;
-using Itinero.IO.Osm;
-using Itinero.Osm.Vehicles;
-using OsmSharp.Osm.PBF.Streams;
-using OsmSharp.Osm.Xml.Streams;
+﻿
+using OsmSharp.Streams;
 using System.IO;
-using System.Linq;
 
 namespace RoutingAlgorithmProject.Utility
 {
@@ -13,37 +9,15 @@ namespace RoutingAlgorithmProject.Utility
 
         public static void ReadOsmData()
         {
-            ReadOsmDataAgain();
-            //using (var fileStream = new FileInfo(@".../.../Resources/north-america-latest.osm.pbf").OpenRead())
-            //{
-            //    var source = new PBFOsmStreamSource(fileStream);
-
-            //    //-83.6752, 36.5408, -75.2418, 39.4659
-
-            //    var filtered = source.FilterBox(-83.6752f, 39.4659f, -75.2418f, 36.5408f); // left, top, right, bottom
-
-            //    var target = new PBFOsmStreamTarget(new FileInfo(@".../.../Resources/Virginia.osm.pbf").Open(FileMode.Create, FileAccess.ReadWrite));
-            //    target.RegisterSource(filtered);
-            //    target.Pull();
-
-            //}
-        }
-
-        private static void ReadOsmDataAgain()
-        {
-            using (var file = new FileInfo(@".../.../Resources/north-america-latest.osm.pbf").OpenRead())
+            string path = "..\\..\\..\\Resources\\district-of-columbia-latest.osm.pbf"; // path to dc data
+            if (File.Exists(path))
             {
-
-                var source = new PBFOsmStreamSource(file);
-
-                //var filtered = source.FilterSpatial(-83.6752f, 39.4659f, -75.2418f, 36.5408f); // left, top, right, bottom
-                //var filtered = from osmGeo in source
-                //               where osmGeo.Type == OsmSharp.Osm.OsmGeoType.Node
-                //               select osmGeo;
-
-                //var complete = filtered.ToList();
-
-
+                using (var fileStreamSource = File.OpenRead(path))
+                {
+                    var source = new PBFOsmStreamSource(fileStreamSource);
+                    var filtered = source.FilterBox(-80f, 42f, -76f, 38f); // left, top, right, bottom
+                    var complete = filtered.ToComplete();
+                }
             }
         }
     }
