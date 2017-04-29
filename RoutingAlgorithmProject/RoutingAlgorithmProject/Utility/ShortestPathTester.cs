47fd878e41f743aab26d73412e13af521d537ca3
somethingd <somethingd@gmail.com>
2017-04-29 03:10:42 -0400

    added more testing

--- RoutingAlgorithmProject/RoutingAlgorithmProject/Utility/ShortestPathTester.cs
+++ RoutingAlgorithmProject/RoutingAlgorithmProject/Utility/ShortestPathTester.cs
@@ -28,22 +28,52 @@ namespace RoutingAlgorithmProject.Utility
             //PathFinder fff = new DijkstraApproximateBucketPathFinder(graph);
             //var a2 = fff.FindShortestPath(a, b);
 
-
+            List<List<TestResults>> results = new List<List<TestResults>>();
             //// get all types that inherit PathFinder
             var pathFinders = typeof(PathFinder).Assembly.GetTypes().Where(t => t.IsSubclassOf(typeof(PathFinder)));
             foreach (Type t in pathFinders)
             {
                 // run TestPathFinder on each type
                 PathFinder pf = (PathFinder)Activator.CreateInstance(t, graph);
-                TestPathFinder(pf);
+                results.Add(TestPathFinder(pf));
             }
 
+            ParseResults(results);
             // TestPathFinder(new AStarApproximateBucketPathFinder(graph));
             //graph.ResetGraph();
-           // TestPathFinder(new DijkstraApproximateBucketPathFinder(graph));
-        //    TestPathFinder(new AStarPathFinder(graph));
-        //    TestPathFinder(new DijkstraMinHeapPathFinder(graph));
-        //    TestPathFinder(new DijkstraPathFinder(graph));
+            // TestPathFinder(new DijkstraApproximateBucketPathFinder(graph));
+            //    TestPathFinder(new AStarPathFinder(graph));
+            //    TestPathFinder(new DijkstraMinHeapPathFinder(graph));
+            //    TestPathFinder(new DijkstraPathFinder(graph));
+        }
+
+        private static void ParseResults(List<List<TestResults>> results)
+        {
+            string outputPath = System.IO.Path.Combine("output", DateTime.Now.ToString("yyyyMMdd_hhmmss") + "TotalResults.txt");
+            if (!System.IO.Directory.Exists("output"))
+                System.IO.Directory.CreateDirectory("output");
+            using (System.IO.TextWriter tw = new System.IO.StreamWriter(outputPath, false))
+            {
+                tw.WriteLine(String.Format("{0,-40} {1,-20} {2,-20}", "PathFinder", "Avg Runtime", "Avg # of nodes"));
+                foreach(var list in results)
+                {
+                    tw.WriteLine(GetResultString(list));  
+                }
+            }
+        }
+
+        private static string GetResultString(List<TestResults> list)
+        {
+            double totalTime = 0;
+            float totalNodes = 0;
+            foreach(var r in list)
+            {
+                totalNodes += r.NumberOfNodes;
+                totalTime += r.Time;
+            }
+            double avgRuntime = totalTime / list.Count;
+            double avgNodes = totalNodes / list.Count;
+            return String.Format("{0,-40} {1,-20} {2,-20}", list.First().PathFinderType.Name, avgRuntime, avgNodes);
         }
 
         static void runTest(Object stateInfo)
@@ -52,52 +82,52 @@ namespace RoutingAlgorithmProject.Utility
             TestPathFinder(pf);
         }
 
+
+
         /// <summary>
         /// reads from a file then finds a path between every point in the file
         /// k points in file = k(k-1)/2 paths
         /// Creates output file for each implementation with the average time for each path
         /// </summary>
         /// <param name="pf"></param>
-        private static void TestPathFinder(PathFinder pf)
+        private static List<TestResults> TestPathFinder(PathFinder pf)
         {
-            try {
-                List<StartDestinationPair> testPoints = ReadPointFile(@"..\..\Resources\dcTestPointssmall.csv");
-                string outputPath = System.IO.Path.Combine("output", DateTime.Now.ToString("yyyyMMdd_hhmmss") + pf.GetType().Name + ".txt");
-                if (!System.IO.Directory.Exists("output"))
-                    System.IO.Directory.CreateDirectory("output");
-                using (System.IO.TextWriter tw = new System.IO.StreamWriter(outputPath, false))
-                {
-                    double totalRunTime = 0;
-                    int successfullPathsFound = 0;
-                    foreach (var startDestinationPair in testPoints)
-                    {
-                        float pathLength = 0;
-                        var sw = new Stopwatch();
-                        sw.Start();
-                        var path = pf.FindShortestPath(startDestinationPair.Start, startDestinationPair.Destination, ref pathLength);
-                        sw.Stop();
-                        pf.ResetGraph();
-                        string msg = null;
-                        double elapsedTime = sw.Elapsed.TotalSeconds;
-                        if (path != null && path.Count > 0)
-                        {
-                            totalRunTime += elapsedTime;
-                            successfullPathsFound++;
-                            msg = startDestinationPair.ToString() + "Completed in " + elapsedTime.ToString() + " seconds." + " with path length = " + pathLength + " vertex count = " + path.Count;
-                        }
-                        else
-                        {
-                            msg = startDestinationPair.ToString() + "Failed to find path in " + elapsedTime.ToString() + " seconds.";
-                        }
-                        tw.WriteLine(pf.GetType().Name + " " + msg );
-                    }
-                    tw.WriteLine("Average Runtime(sec) = " + totalRunTime / successfullPathsFound + " Failed Paths = " + (testPoints.Count-successfullPathsFound));
-                }
-            }
-            catch (Exception ex)
+            List<TestResults> resultsList = new List<TestResults>();
+            List<StartDestinationPair> testPoints = ReadPointFile(@"..\..\Resources\dcTestPoints.csv");
+            string outputPath = System.IO.Path.Combine("output", DateTime.Now.ToString("yyyyMMdd_hhmmss") + pf.GetType().Name + ".txt");
+            if (!System.IO.Directory.Exists("output"))
+                System.IO.Directory.CreateDirectory("output");
+            using (System.IO.TextWriter tw = new System.IO.StreamWriter(outputPath, false))
             {
-                var x = 1;
+                double totalRunTime = 0;
+                int successfullPathsFound = 0;
+                foreach (var startDestinationPair in testPoints)
+                {
+                    float pathLength = 0;
+                    var sw = new Stopwatch();
+                    sw.Start();
+                    var path = pf.FindShortestPath(startDestinationPair.Start, startDestinationPair.Destination, ref pathLength);
+                    sw.Stop();
+                    pf.ResetGraph();
+                    string msg = null;
+                    double elapsedTime = sw.Elapsed.TotalSeconds;
+                    if (path != null && path.Count > 0)
+                    {
+                        totalRunTime += elapsedTime;
+                        successfullPathsFound++;
+                        msg = startDestinationPair.ToString() + "Completed in " + elapsedTime.ToString() + " seconds." + " with path length = " + pathLength + " vertex count = " + path.Count;
+                    }
+                    else
+                    {
+                        msg = startDestinationPair.ToString() + "Failed to find path in " + elapsedTime.ToString() + " seconds.";
+                    }
+                    tw.WriteLine(pf.GetType().Name + " " + msg);
+                    if(path!=null)
+                        resultsList.Add(new TestResults(elapsedTime, path.Count, pf.GetType()));
+                }
+                tw.WriteLine("Average Runtime(sec) = " + totalRunTime / successfullPathsFound + " Failed Paths = " + (testPoints.Count - successfullPathsFound));
             }
+            return resultsList;
         }
 
 
@@ -109,7 +139,8 @@ namespace RoutingAlgorithmProject.Utility
             using (System.IO.TextReader tr = new System.IO.StreamReader(v))
             {
                 string line;
-                while ((line = tr.ReadLine()) != null){
+                while ((line = tr.ReadLine()) != null)
+                {
                     testPoints.Add(parseCoordinate(line));
                 }
             }
@@ -119,9 +150,9 @@ namespace RoutingAlgorithmProject.Utility
         private static List<StartDestinationPair> CreateStartDestinationPair(List<Coordinates> testPoints)
         {
             List<StartDestinationPair> pairs = new List<StartDestinationPair>();
-            for (int i = 0; i < testPoints.Count-1; i++)
+            for (int i = 0; i < testPoints.Count - 1; i++)
             {
-                for (int j = i+1; j < testPoints.Count; j++)
+                for (int j = i + 1; j < testPoints.Count; j++)
                 {
                     pairs.Add(new StartDestinationPair(testPoints[i], testPoints[j]));
                 }
@@ -152,7 +183,22 @@ namespace RoutingAlgorithmProject.Utility
                 return Start.ToString() + "->" + Destination.ToString();
             }
 
-            
+
+        }
+
+        public class TestResults
+        {
+            public readonly double Time;
+            public readonly int NumberOfNodes;
+            public readonly Type PathFinderType;
+
+            public TestResults(double time, int nodes, Type pf)
+            {
+                this.Time = time;
+                this.NumberOfNodes = nodes;
+                this.PathFinderType = pf;
+            }
+
         }
     }
 }
